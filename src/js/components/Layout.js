59dfa55e4be19bbd18d9a306a84114a46ac5e6a6
Ramitha Abeyratne <ramithaabeyratne@rocketmail.com>
2017-01-31 21:44:24 +0530

    Added the basic tic tac toe board UI

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -5,7 +5,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <h1>Sample Text</h1>
+        <h1>REACT-JS GAME</h1>
       </div>
     );
   }
